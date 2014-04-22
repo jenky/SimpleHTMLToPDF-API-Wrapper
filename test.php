7cc8a83d52b99d9cc69070175cfbfaa4e008440f
Baudouin Feildel <baudouin@feildel.org>
2014-04-22 10:19:03 +0200

    Update test page, to show how to use the wrapper.

--- test.php
+++ test.php
@@ -2,10 +2,22 @@
 /*
  * This is just a test page for the API Wrapper.
  */
-if(isset($_POST['url'])) {
+if(isset($_POST['url']) || isset($_POST['function'])) {
 	include("simplehtmltopdf-api.php");
 
 	$api = new SimpleHTMLToPDF();
+
+	if($_POST['function'] == "display") {
+		$api->display(urldecode($_POST['url']),
+			$_POST['orientation'],
+			array(
+				$_POST['mtop'],
+				$_POST['mleft'],
+				$_POST['mright'],
+				$_POST['mbot'],
+		));
+	}
+
 
 	$api->display(urldecode($_POST["url"]));
 } else {
@@ -21,6 +33,23 @@ if(isset($_POST['url'])) {
 			<label for="url">Get this page : </label>
 			<input type="text" name="url" id="url">
 			<input type="submit" value="Go !">
+		</form>
+
+		<form method="POST" action="">
+			<input type="hidden" name="function" value="display">
+			<label for="url">SimpleHTMLToPDF::display(</label>
+			<input type="text" name="url" id="url">
+			<label for="orientation">, orientation = </label>
+			<select name="orientation" id="orientation">
+				<option>Portrait</option>
+				<option>Landscape</option>
+			</select>
+			<label for="margins">, margins = array(</label>
+			<input type="number" name="mtop" placeholder="Top" id="margins" value="10">,
+			<input type="number" name="mleft" placeholder="Left" value="10">,
+			<input type="number" name="mright" placeholder="Right" value="10">,
+			<input type="number" name="mbot" placeholder="Bottom" value="10">));
+			<input type="submit" value="Send Request">
 		</form>
 	</body>
 </html>
