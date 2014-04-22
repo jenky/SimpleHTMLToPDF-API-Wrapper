f5c0b480bfbf6caafa6088eb5c6a8666dff238b6
Baudouin Feildel <baudouin@feildel.org>
2014-04-22 10:34:15 +0200

    Add download function to the test page.

--- test.php
+++ test.php
@@ -7,19 +7,37 @@ if(isset($_POST['url']) || isset($_POST['function'])) {
 
 	$api = new SimpleHTMLToPDF();
 
-	if($_POST['function'] == "display") {
-		$api->display(urldecode($_POST['url']),
-			$_POST['orientation'],
-			array(
-				$_POST['mtop'],
-				$_POST['mleft'],
-				$_POST['mright'],
-				$_POST['mbot'],
-		));
+	switch ($_POST['function']) {
+		case 'display':
+			$api->display(
+				urldecode($_POST['url']),
+				$_POST['orientation'],
+				array(
+					$_POST['mtop'],
+					$_POST['mleft'],
+					$_POST['mright'],
+					$_POST['mbot'],
+			));
+			break;
+
+		case 'download':
+			$api->download(
+				urldecode($_POST['url']),
+				$_POST['orientation'],
+				array(
+					$_POST['mtop'],
+					$_POST['mleft'],
+					$_POST['mright'],
+					$_POST['mbot'],
+			));
+			break;
+
+		default:
+			$api->display(urldecode($_POST["url"]));
+			break;
 	}
 
 
-	$api->display(urldecode($_POST["url"]));
 } else {
 ?>
 <!DOCTYPE html>
@@ -29,31 +47,64 @@ if(isset($_POST['url']) || isset($_POST['function'])) {
 		<title>Test page for Simple HTML to PDF Wrapper</title>
 	</head>
 	<body>
-		<form method="POST" action="">
+		<!--<form method="POST" action="">
 			<label for="url">Get this page : </label>
 			<input type="text" name="url" id="url">
 			<input type="submit" value="Go !">
-		</form>
-		<br>
+		</form>-->
+		<h1>SimpleHTMLToPDF API Wrapper test page</h1>
+		<p>
+			This page help you understand how to use this API Wrapper.<br>
+			Each form show you how to use each function and what parameters you should pass to these.<br>
+			Play with it and be sure to check the GitHub repository : <a href="https://github.com/AMDG2/SimpleHTMLToPDF-API-Wrapper">https://github.com/AMDG2/SimpleHTMLToPDF-API-Wrapper</a>
+		</p>
 		<form method="POST" action="">
-			<input type="hidden" name="function" value="display">
-			<label for="url">SimpleHTMLToPDF::display(</label>
-			<input type="text" name="url" id="url" placeholder="http://www.simplehtmltopdf.com/" required>
-			<label for="orientation">, orientation = </label>
-			<select name="orientation" id="orientation">
-				<option>Portrait</option>
-				<option>Landscape</option>
-			</select>
-			, margins = array(
-			<label for="mtop">Top = </label>
-			<input type="number" name="mtop" id="mtop" placeholder="Top" style="width: 3em;" value="10">,
-			<label for="mleft">Left = </label>
-			<input type="number" name="mleft" id="mleft" placeholder="Left" style="width: 3em;" value="10">,
-			<label for="mright">Right = </label>
-			<input type="number" name="mright" id="mright" placeholder="Right" style="width: 3em;" value="10">,
-			<label for="mbot">Bottom = </label>
-			<input type="number" name="mbot" id="mbot" placeholder="Bottom" style="width: 3em;" value="10">));
-			<input type="submit" value="Send Request">
+			<fieldset><legend>The display function</legend>
+				<input type="hidden" name="function" value="display">
+				<pre>include("simplehtmltopdf-api.php");
+$api = new SimpleHTMLToPDF();</pre>
+				<label for="url">$api->display(</label>
+				<input type="text" name="url" id="url" placeholder="http://www.simplehtmltopdf.com/" required>
+				<label for="orientation">, orientation = </label>
+				<select name="orientation" id="orientation">
+					<option>Portrait</option>
+					<option>Landscape</option>
+				</select>
+				, margins = array(
+				<label for="mtop">Top = </label>
+				<input type="number" name="mtop" id="mtop" placeholder="Top" style="width: 3em;" value="10">,
+				<label for="mleft">Left = </label>
+				<input type="number" name="mleft" id="mleft" placeholder="Left" style="width: 3em;" value="10">,
+				<label for="mright">Right = </label>
+				<input type="number" name="mright" id="mright" placeholder="Right" style="width: 3em;" value="10">,
+				<label for="mbot">Bottom = </label>
+				<input type="number" name="mbot" id="mbot" placeholder="Bottom" style="width: 3em;" value="10">));
+				<input type="submit" value="Send Request">
+			</fieldset>
+		</form>
+		<form method="POST" action="">
+			<fieldset><legend>The download function</legend>
+				<input type="hidden" name="function" value="download">
+				<pre>include("simplehtmltopdf-api.php");
+$api = new SimpleHTMLToPDF();</pre>
+				<label for="url">$api->download(</label>
+				<input type="text" name="url" id="url" placeholder="http://www.simplehtmltopdf.com/" required>
+				<label for="orientation">, orientation = </label>
+				<select name="orientation" id="orientation">
+					<option>Portrait</option>
+					<option>Landscape</option>
+				</select>
+				, margins = array(
+				<label for="mtop">Top = </label>
+				<input type="number" name="mtop" id="mtop" placeholder="Top" style="width: 3em;" value="10">,
+				<label for="mleft">Left = </label>
+				<input type="number" name="mleft" id="mleft" placeholder="Left" style="width: 3em;" value="10">,
+				<label for="mright">Right = </label>
+				<input type="number" name="mright" id="mright" placeholder="Right" style="width: 3em;" value="10">,
+				<label for="mbot">Bottom = </label>
+				<input type="number" name="mbot" id="mbot" placeholder="Bottom" style="width: 3em;" value="10">));
+				<input type="submit" value="Send Request">
+			</fieldset>
 		</form>
 	</body>
 </html>
