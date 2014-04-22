daa15ba977905983a994ae18f9693658a9932291
Baudouin Feildel <baudouin@feildel.org>
2014-04-22 09:35:04 +0200

    Add margins parameters.

--- simplehtmltopdf-api.php
+++ simplehtmltopdf-api.php
@@ -4,11 +4,19 @@ class SimpleHTMLToPDF {
 
 	private $baseUrl = "http://api.simplehtmltopdf.com/";
 
-	public function get($url, $orientation = "Portrait") {
+	public function get($url, $orientation = "Portrait", $marges = array(10,10,10,10)) {
 
 		$orientation = ($orientation == "Landscape") ? $orientation : "Portrait";
 
 		$requestUrl = $this->baseUrl . "?link=".urlencode($url)."&amp;orientation=". $orientation;
+
+		if(!count($marges) == 4 || !is_array($marges))
+			$marges = array(10,10,10,10);
+
+		$requestUrl .= "&amp;mtop="   . $marges[0];
+		$requestUrl .= "&amp;mleft="  . $marges[1];
+		$requestUrl .= "&amp;mright=" . $marges[2];
+		$requestUrl .= "&amp;mbot="   . $marges[3];
 
 		$curl = curl_init($requestUrl);
 		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
@@ -21,23 +29,23 @@ class SimpleHTMLToPDF {
 		return $result;
 	}
 
-	public function download($url, $orientation = "Portrait") {
+	public function download($url, $orientation = "Portrait", $marges = array(10,10,10,10)) {
 
 		header("Content-Type: application/pdf");
 		header("Cache-Control: no-cache");
 		header("Accept-Ranges: none");
 		header("Content-Disposition: attachment; filename=\"your_pdf_name.pdf\"");
 
-		echo $this->get($url, $orientation);
+		echo $this->get($url, $orientation, $marges);
 	}
 
-	public function display($url, $orientation = "Portrait") {
+	public function display($url, $orientation = "Portrait", $marges = array(10,10,10,10)) {
 
 		header("Content-Type: application/pdf");
 		header("Cache-Control: no-cache");
 		header("Accept-Ranges: none");
 
-		echo $this->get($url, $orientation);
+		echo $this->get($url, $orientation, $marges);
 	}
 }
 
